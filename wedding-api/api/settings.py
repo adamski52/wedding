d8381768a7804178fe6136a28db3c3ad50f5a672
Jonathan Adamski <me@jonathanadamski.com>
2018-07-22 01:00:13 -0400

    fixing port conflict.

--- wedding-api/api/settings.py
+++ wedding-api/api/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432')
+        'PORT': os.getenv('DB_PORT', '5433')
     }
 }
 
